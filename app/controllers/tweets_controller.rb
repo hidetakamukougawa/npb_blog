1e0caaf73db8e447fc0f83f1c0c48ac74b33331c
向川　英孝 <jinsino1987@gmail.com>
2019-06-30 18:13:54 +0900

    Update tweets_controller.rb
    新規投稿のためのアクションを追記。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,5 +3,17 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :image, :text).merge(user_id: current_user.id)
   end
 end

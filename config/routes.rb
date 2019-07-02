644e1d50557b7a7d0b87f215f1fdc90dd56dcbd9
向川　英孝 <jinsino1987@gmail.com>
2019-07-02 16:31:10 +0900

    Update routes.rb
    投稿詳細ページに遷移するようにルーティングを追記。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets, only: [:index, :new, :create, :edit, :update]
+  resources :tweets, only: [:index, :new, :create, :edit, :update, :show] do
+    resources :comments, only: [:new, :create]
+  end
 end

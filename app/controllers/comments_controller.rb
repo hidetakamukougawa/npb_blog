9a56ad5db2b08c72960fef191237533b54482759
向川　英孝 <jinsino1987@gmail.com>
2019-07-02 16:26:01 +0900

    Create comments_controller.rb
    投稿に対するコメントを投稿できるようにアクションを追加。

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/tweets/#{comment.tweet.id}"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(tweet_id: params[:tweet_id], user_id: current_user.id)
+  end
+end

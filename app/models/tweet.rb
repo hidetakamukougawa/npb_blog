ccc65e1ec070ad74f5997a6d3604a99eb5e99845
向川　英孝 <jinsino1987@gmail.com>
2019-07-03 17:36:12 +0900

    Update tweet.rb
    ツイート作成時、編集時のバリデーションを追記した。

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,15 @@ class Tweet < ApplicationRecord
   has_many :comments
   belongs_to :user
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :text, presence: true
+  # validates :image, presence: true
+  validate :image_presence
+
+  private
+
+  def image_presence
+    errors.add(:image) unless image.attached?
+  end
 end

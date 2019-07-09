6f86027ae08823ecee7f1e392437f4fc598f56cf
向川　英孝 <jinsino1987@gmail.com>
2019-07-09 17:36:43 +0900

    Create tweets.rb
    Tweetに関するFactoryBotを作成。

---
+++ spec/factories/tweets.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+
+  factory :tweet do
+    title      {"柳田すごすぎ"}
+    text       {"パワーありすぎ！！"}
+    after(:build) do |tweet|
+      tweet.image.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'htmlcss.jpg')), filename: 'htmlcss.jpg', content_type: 'image/jpeg')
+    end
+    user
+  end
+end

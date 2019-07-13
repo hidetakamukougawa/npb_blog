ca3ef6a0f58dd860df392d48db8308c922068746
向川　英孝 <jinsino1987@gmail.com>
2019-07-13 21:47:42 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.11.0"
 
 set :application, "npb_blog"
 
-set :repo_url, "git@example.com:hidetakamukougawa/npb_blog.git"
+set :repo_url, "git@github.com:hidetakamukougawa/npb_blog.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 

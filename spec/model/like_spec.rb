65a4069b03c8b67d742db513bb72c10bc28cbb7c
向川　英孝 <jinsino1987@gmail.com>
2019-07-19 15:54:13 +0900

    Update like_spec.rb
    慣習に従う記述に変更。

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
-describe Like, type: :model do
-  before do
-    @user = FactoryBot.create(:user)
-    @other = FactoryBot.create(:user)
-    @tweet = @other.tweets.create
-  end
+RSpec.describe Like, type: :model do
+  describe '#create' do
+    before do
+      @user = create(:user)
+      @other = create(:user)
+      @tweet = @other.tweets.create
+    end
 
-  it "is valid with user and tweet" do
-    like = Like.new(user: @user, tweet: @tweet)
-    expect(like).to be_valid
-  end
+    it "is valid with user and tweet" do
+      like = Like.new(user: @user, tweet: @tweet)
+      expect(like).to be_valid
+    end
 
-  it "is invalid with duplicate" do
-    Like.create(user: @user, tweet: @tweet)
-    like = Like.new(user: @user, tweet: @tweet)
-    expect(like).not_to be_valid
+    it "is invalid with duplicate" do
+      Like.create(user: @user, tweet: @tweet)
+      like = Like.new(user: @user, tweet: @tweet)
+      expect(like).not_to be_valid
+    end
   end
-
 end

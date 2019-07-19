168952faf20501e8921778d5829bf9c69628c616
向川　英孝 <jinsino1987@gmail.com>
2019-07-19 10:36:19 +0900

    Update routes.rb
    いいね機能を実装するため追記。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets, only: [:index, :new, :create, :edit, :update, :show, :destroy] do
+  # resources :tweets, only: [:index, :new, :create, :edit, :update, :show, :destroy] do
+  resources :tweets do
     resources :comments, only: [:new, :create]
+    resources :likes, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show]
 end

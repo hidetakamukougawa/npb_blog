510549b7b3cb2aea77704d519096306ffcac8368
向川　英孝 <jinsino1987@gmail.com>
2019-08-25 14:18:28 +0900

    Update tweets.rb

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,8 +1,9 @@
-FactoryBot.define do
+# frozen_string_literal: true
 
+FactoryBot.define do
   factory :tweet do
-    title      {"柳田すごすぎ"}
-    text       {"パワーありすぎ！！"}
+    title      { '柳田すごすぎ' }
+    text       { 'パワーありすぎ！！' }
     after(:build) do |tweet|
       tweet.image.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'htmlcss.jpg')), filename: 'htmlcss.jpg', content_type: 'image/jpeg')
     end

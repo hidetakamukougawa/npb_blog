a4c7dd6548cdc9ab92d13ebbd501909d65329f60
向川　英孝 <jinsino1987@gmail.com>
2019-08-25 14:17:39 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
   # resources :tweets, only: [:index, :new, :create, :edit, :update, :show, :destroy] do
   resources :tweets do
-    resources :comments, only: [:new, :create]
-    resources :likes, only: [:create, :destroy]
+    resources :comments, only: %i[new create]
+    resources :likes, only: %i[create destroy]
   end
-  resources :users, only: [:index, :show]
+  resources :users, only: %i[index show]
 end

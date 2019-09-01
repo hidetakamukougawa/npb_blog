9457ffe39dad37a6f8d11ae0b93b28fe59a4fee9
向川　英孝 <jinsino1987@gmail.com>
2019-09-01 15:15:17 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -18,6 +19,7 @@ module NpbBlog
     end
     # Initialize configuration defaults for originally generated Rails version.
     # config.load_defaults 5.2
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
